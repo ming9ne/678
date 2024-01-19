373a1a5e583cbf55aaac34b8b04787ce6e7baabe
박태현 <lcecoffee@naver.com>
2024-01-19 12:06:10 +0900

    Feat : 커밋 테스트3
    01.19 12:06 커밋 테스트3

---
+++ crud/src/main/java/com/sw678/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.sw678.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudApplication.class, args);
+    }
+
+}
---
+++ crud/src/main/java/com/sw678/crud/test.java
@@ -0,0 +1,5 @@
+package com.sw678.crud;
+
+public class test {
+    test crud
+}
