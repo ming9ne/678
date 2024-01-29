273780caf93b8c8c5eab897d22f7e4d06870d726
박태현 <lcecoffee@naver.com>
2024-01-29 14:27:13 +0900

    Feat : 지도화면 ver1
    01.29 14:27
    
    지도화면 ver1

--- app/src/views/examples/Maps.js
+++ app/src/views/examples/Maps.js
@@ -7,9 +7,10 @@ const { kakao } = window;
 function KakaoMapWithMarker() {
     const [parkData, setParkData] = useState([]);
     const [selectedPark, setSelectedPark] = useState(null);
+    const [pollutionData, setPollutionData] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchParkData = async () => {
             try {
                 const response = await fetch(
                     "http://192.168.219.107:8080/api/v1/park-service/load"
@@ -25,11 +26,28 @@ function KakaoMapWithMarker() {
             }
         };
 
-        fetchData();
+        const fetchPollutionData = async () => {
+            try {
+                const response = await fetch(
+                    "http://192.168.219.107:8080/api/v1/pollution-service/load"
+                );
+                if (response.ok) {
+                    const data = await response.json();
+                    setPollutionData(data);
+                } else {
+                    console.error("Failed to fetch pollution data");
+                }
+            } catch (error) {
+                console.error("Error while fetching pollution data", error);
+            }
+        };
+
+        fetchParkData();
+        fetchPollutionData();
     }, []);
 
     useEffect(() => {
-        if (parkData.length > 0) {
+        if (parkData.length > 0 && pollutionData.length > 0) {
             const mapContainer = document.getElementById("pollution-map");
             const mapOption = {
                 center: new kakao.maps.LatLng(37.566826, 126.9786567),
@@ -38,19 +56,40 @@ function KakaoMapWithMarker() {
 
             const map = new kakao.maps.Map(mapContainer, mapOption);
 
-            // 행정구역 표시 부분
+            // 행정구역 표시 부분 수정
             let data = geojson.features;
-            let coordinates = [];
-            let name = "";
 
             const customOverlay = new kakao.maps.CustomOverlay({});
             const infowindow = new kakao.maps.InfoWindow({ removable: true });
 
-            const displayArea = (coordinates, name) => {
+            const displayArea = (coordinates, areaData) => {
                 let path = [];
                 coordinates[0].forEach((coordinate) => {
                     path.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
                 });
+
+                let fillColor = "#fff"; // Default color
+
+                switch (areaData.grade) {
+                    case "매우 나쁨":
+                        fillColor = "#FF6666"; // Light Red
+                        break;
+                    case "나쁨":
+                        fillColor = "#FFD1D1"; // Light Yellow
+                        break;
+                    case "보통":
+                        fillColor = "#C0E4C9"; // Light Green
+                        break;
+                    case "좋음":
+                        fillColor = "#B3CCFF"; // Light Blue
+                        break;
+                    case "점검중":
+                        fillColor = "#EAEAEA"; // Light Gray
+                        break;
+                    default:
+                        break;
+                }
+
 
                 let polygon = new kakao.maps.Polygon({
                     map: map,
@@ -58,33 +97,16 @@ function KakaoMapWithMarker() {
                     strokeWeight: 2,
                     strokeColor: "#004c80",
                     strokeOpacity: 0.8,
-                    fillColor: "#fff",
+                    fillColor: fillColor,
                     fillOpacity: 0.7,
-                });
-
-                kakao.maps.event.addListener(polygon, "mouseover", function (mouseEvent) {
-                    polygon.setOptions({ fillColor: "#09f" });
-                    customOverlay.setContent('<div class="area">' + name + "</div>");
-                    customOverlay.setPosition(mouseEvent.latLng);
-                    customOverlay.setMap(map);
-                });
-
-                kakao.maps.event.addListener(polygon, "mousemove", function (mouseEvent) {
-                    customOverlay.setPosition(mouseEvent.latLng);
-                });
-
-                kakao.maps.event.addListener(polygon, "mouseout", function () {
-                    polygon.setOptions({ fillColor: "#fff" });
-                    customOverlay.setMap(null);
                 });
 
                 kakao.maps.event.addListener(polygon, "click", function (mouseEvent) {
                     const content =
-                        '<div style="padding:2px;"><p><b>' +
-                        name +
-                        '</b></p><p>미세먼지: ' +
-                        '</b></p><p>초미세먼지: ' +
-                        "</div>";
+                        `<div style="padding:2px;"><p><b>${areaData.msrName}</b></p>` +
+                        `<p>Grade: ${areaData.grade}</p>` +
+                        `<p>미세먼지: ${areaData.pm10}</p>` +
+                        `<p>초미세먼지: ${areaData.pm25}</p></div>`;
 
                     infowindow.setContent(content);
                     infowindow.setPosition(mouseEvent.latLng);
@@ -93,9 +115,14 @@ function KakaoMapWithMarker() {
             };
 
             data.forEach((val) => {
-                coordinates = val.geometry.coordinates;
-                name = val.properties.SIG_KOR_NM;
-                displayArea(coordinates, name);
+                const coordinates = val.geometry.coordinates;
+                const areaData = pollutionData.find(
+                    (pollution) => pollution.msrName === val.properties.SIG_KOR_NM
+                );
+
+                if (areaData) {
+                    displayArea(coordinates, areaData);
+                }
             });
 
             // 공원 마커 표시 부분
@@ -116,14 +143,15 @@ function KakaoMapWithMarker() {
                 });
             });
         }
-    }, [parkData]);
+    }, [parkData, pollutionData]);
 
     useEffect(() => {
         // 선택된 공원이 변경될 때 실행되는 효과
         if (selectedPark) {
             // 여기에서 상세 정보를 표시하는 모달 또는 다른 방식으로 구현
             alert(
-                `Selected Park: ${selectedPark.parkName}\nAddress: ${selectedPark.parkAddr}\nTel: ${selectedPark.telNum}`
+                `공원 이름: ${selectedPark.parkName}\n공원 주소: ${selectedPark.parkAddr}\n전화번호: ${selectedPark.telNum}
+                \n인터넷 주소: ${selectedPark.parkUrl}\n${selectedPark.parkImg}`
             );
         }
     }, [selectedPark]);
@@ -140,3 +168,4 @@ function KakaoMapWithMarker() {
 }
 
 export default KakaoMapWithMarker;
+
