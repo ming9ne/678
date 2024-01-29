792a97c762c5a5bd3566bd826ea68432724d22a7
박태현 <lcecoffee@naver.com>
2024-01-29 12:44:35 +0900

    Feat : APP 지도 작업
    01.29 12:44
    
    APP 폴리곤 작업

--- app/src/views/examples/Maps.js
+++ app/src/views/examples/Maps.js
@@ -1,48 +1,92 @@
 import React, { useEffect } from "react";
+import geojson from '../../data/geo3.json'; // 경로를 적절히 조정하세요
 
 const { kakao } = window;
 
 function KakaoMapWithMarker() {
-  useEffect(() => {
-    // 카카오 맵 API 로드
-    kakao.maps.load(() => {
-      const container = document.getElementById('map');
-      const options = {
-        center: new kakao.maps.LatLng(37.615749261729, 127.01326329513556),
-        level: 3
-      };
+    useEffect(() => {
+        let data = geojson.features;
+        let coordinates = [];
+        let name = '';
+        const mapContainer = document.getElementById('pollution-map');
+        const mapOption = {
+            center: new kakao.maps.LatLng(37.566826, 126.9786567),
+            level: 9,
+        };
 
-      // 지도 생성
-      const map = new kakao.maps.Map(container, options);
+        const map = new kakao.maps.Map(mapContainer, mapOption);
+        const customOverlay = new kakao.maps.CustomOverlay({});
+        const infowindow = new kakao.maps.InfoWindow({ removable: true });
 
-      // 마커 생성
-      const markerPosition = new kakao.maps.LatLng(37.615749261729, 127.01326329513556);
-      const marker = new kakao.maps.Marker({
-        position: markerPosition
-      });
+        const displayArea = (coordinates, name) => {
+            let path = [];
+            let points = [];
 
-      // 마커 지도에 표시
-      marker.setMap(map);
+            // pollution 변수를 사용하는 부분 삭제
 
-      // 창 크기가 변경될 때 맵을 중앙에 유지
-      const centerPosition = map.getCenter();
-      const resizeEvent = kakao.maps.event.addListener(map, 'resize', function () {
-        map.panTo(centerPosition);
-      });
+            coordinates[0].forEach((coordinate) => {
+                let point = {};
+                point.x = coordinate[1];
+                point.y = coordinate[0];
+                points.push(point);
+                path.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
+            });
 
-      // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
-      return () => {
-        kakao.maps.event.removeListener(resizeEvent);
-      };
-    });
-  }, []);
+            let polygon = new kakao.maps.Polygon({
+                map: map,
+                path: path,
+                strokeWeight: 2,
+                strokeColor: '#004c80',
+                strokeOpacity: 0.8,
+                fillColor: '#fff',
+                fillOpacity: 0.7,
+            });
 
-  return (
-      <div id="map" style={{
-        width: '100vw', // 전체 화면 가로
-        height: '100vh', // 전체 화면 세로
-      }}></div>
-  );
+            kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
+                polygon.setOptions({ fillColor: '#09f' });
+                customOverlay.setContent('<div class="area">' + name + '</div>');
+                customOverlay.setPosition(mouseEvent.latLng);
+                customOverlay.setMap(map);
+            });
+
+            kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
+                customOverlay.setPosition(mouseEvent.latLng);
+            });
+
+            kakao.maps.event.addListener(polygon, 'mouseout', function () {
+                polygon.setOptions({ fillColor: '#fff' });
+                customOverlay.setMap(null);
+            });
+
+            kakao.maps.event.addListener(polygon, 'click', function (mouseEvent) {
+                // pollution 변수를 사용하는 부분 삭제
+
+                const content =
+                    '<div style="padding:2px;"><p><b>' +
+                    name +
+                    '</b></p><p>미세먼지: ' +
+                    '</b></p><p>초미세먼지: ' +
+                    '</div>';
+
+                infowindow.setContent(content);
+                infowindow.setPosition(mouseEvent.latLng);
+                infowindow.setMap(map);
+            });
+        };
+
+        data.forEach((val) => {
+            coordinates = val.geometry.coordinates;
+            name = val.properties.SIG_KOR_NM;
+            displayArea(coordinates, name);
+        });
+    }, []);
+
+    return (
+        <div id="pollution-map" style={{
+            width: '100vw',
+            height: '100vh',
+        }}></div>
+    );
 }
 
 export default KakaoMapWithMarker;
