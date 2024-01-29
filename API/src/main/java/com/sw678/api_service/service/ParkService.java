0611c211b2fa372d4b64275f8abc6c80e3bb419c
tns250cpp <tns250cpp@naver.com>
2024-01-29 15:03:31 +0900

    Feat : Service 패키지 예외처리 추가

--- API/src/main/java/com/sw678/api_service/service/ParkService.java
+++ API/src/main/java/com/sw678/api_service/service/ParkService.java
@@ -9,6 +9,8 @@ import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,12 +20,13 @@ import java.util.List;
 
 @Service
 public class ParkService {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final Logger log = LoggerFactory.getLogger(getClass());
     private final RestTemplate restTemplate;
+    private static final String RESULT_CODE = "INFO-000";
+    private List<ParkDto> parkDtoList = new ArrayList<>();
+    private HttpStatusCode status;
     private ParkRepository parkRepository;
-    List<ParkDto> parkDtoList;
 
     @Autowired
     public ParkService(RestTemplate restTemplate, ParkRepository parkRepository) {
@@ -31,51 +34,70 @@ public class ParkService {
         this.parkRepository = parkRepository;
     }
 
-    public List<ParkDto> saveParkInfo(){
+    public List<ParkDto> saveParkInfo() {
         int startPage = 1;
         int maxPage = 500;
         String url = "http://openapi.seoul.go.kr:8088/5051666379746e733737476265586d/json/SearchParkInfoService/"
                 + startPage + "/" + maxPage;
 
         try {
-            log.info("\n=== api 호출 후 데이터 저장 로직 시작 ===\n");
-            parkDtoList = new ArrayList<>();
+            parkDtoList.clear();
+            log.info("\n========= parkDtoList 초기화 ===========");
+            log.info("\n========= 공원 api 호출 후 데이터 저장 로직 시작 =========");
+
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+            status = responseEntity.getStatusCode();
+            if (status == HttpStatus.OK
+                    || status == HttpStatus.CREATED
+                    || status == HttpStatus.ACCEPTED) { // 비동기 callback 아직 처리안함
 
-            // API 호출 후 JSON 데이터 파싱
-            JSONObject jsonObject = (JSONObject) new JSONParser().parse(responseEntity.getBody());
-            JSONObject jsonData = (JSONObject) jsonObject.get("SearchParkInfoService");
-            JSONArray totalData = (JSONArray) jsonData.get("row");
-            //System.out.println(totalData);
+                System.out.println("HttpStatus : " + status);
 
+                // API 호출 후 JSON 데이터 파싱
+                JSONObject jsonObject = (JSONObject) new JSONParser().parse(responseEntity.getBody());
+                JSONObject jsonData = (JSONObject) jsonObject.get("SearchParkInfoService");
+                JSONObject jsonResult = (JSONObject) jsonData.get("RESULT");
+                JSONArray totalData = (JSONArray) jsonData.get("row");
 
-            for(Object data : totalData){
-                JSONObject object = (JSONObject) data;
-                ParkDto parkDto = makeDtoByJsonData(object);
-                parkDtoList.add(parkDto);
-                parkRepository.save(parkDto.toEntity());
+                String code = (String) jsonResult.get("CODE");
+
+                if (jsonObject != null && code.equals(RESULT_CODE)) {
+                    for (Object data : totalData) {
+                        JSONObject object = (JSONObject) data;
+                        ParkDto parkDto = makeDtoByJsonData(object);
+                        parkDtoList.add(parkDto);
+                        parkRepository.save(parkDto.toEntity());
+                    }
+                } else {
+                    log.warn("API 호출 실패");
+                    System.out.println("요청 결과 데이터 오류. 코드값 : " + code);
+                    return null;
+                }
+            } else {
+                log.warn("API 호출 실패");
+                System.out.println("API 호출 실패. 상태코드 : " + status);
+                return null;
             }
         } catch (Exception e) {
             log.warn("초기 API 호출 중 오류 발생");
-            e.printStackTrace();
+            System.out.println("오류 메시지 : " + e.getMessage());
+            return null;
         }
         log.info("\n=== api 호출 후 데이터 저장 로직 끝 ===");
         return parkDtoList;
     }
 
-
-
-    private static ParkDto makeDtoByJsonData(JSONObject park){
+    private static ParkDto makeDtoByJsonData(JSONObject park) {
         ParkDto parkDto = ParkDto.builder()
-                            .parkId(Long.parseLong((String) park.get("P_IDX")))
-                            .parkName((String) park.get("P_PARK"))
-                            .parkAddr((String) park.get("P_ADDR"))
-                            .telNum((String) park.get("P_ADMINTEL"))
-                            .parkLocX((String)park.get("G_LONGITUDE"))
-                            .parkLocY((String)park.get("G_LATITUDE"))
-                            .parkImg((String) park.get("P_IMG"))
-                            .parkUrl((String) park.get("TEMPLATE_URL"))
-                            .build();
+                .parkId(Long.parseLong((String) park.get("P_IDX")))
+                .parkName((String) park.get("P_PARK"))
+                .parkAddr((String) park.get("P_ADDR"))
+                .telNum((String) park.get("P_ADMINTEL"))
+                .parkLocX((String) park.get("G_LONGITUDE"))
+                .parkLocY((String) park.get("G_LATITUDE"))
+                .parkImg((String) park.get("P_IMG"))
+                .parkUrl((String) park.get("TEMPLATE_URL"))
+                .build();
         return parkDto;
     }
 }
--- API/src/main/java/com/sw678/api_service/service/PollutionService.java
+++ API/src/main/java/com/sw678/api_service/service/PollutionService.java
@@ -9,6 +9,8 @@ import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -22,59 +24,82 @@ import java.util.List;
 @Service
 public class PollutionService {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String RESULT_CODE = "INFO-000";
+    private final Logger log = LoggerFactory.getLogger(getClass());
     private final RestTemplate restTemplate;
     private PollutionRepository pollutionRepository;
-    List<PollutionDto> pollutionDtoList = new ArrayList<>();
+    private List<PollutionDto> pollutionDtoList = new ArrayList<>();
+    private HttpStatusCode status;
 
     @Autowired
     public PollutionService(RestTemplate restTemplate, PollutionRepository pollutionRepository) {
         this.restTemplate = restTemplate;
         this.pollutionRepository = pollutionRepository;
-        // 서비스 생성 단계에서 기존의 데이터를 갖고와야 할듯
-        // => 미세먼지, 점검 중 등의 데이터가 올바르지 않을 때 그전의 데이터 값을 보여줌
     }
 
-    @Scheduled(fixedDelay = 300000)
+    @Scheduled(fixedDelay = 600000)
     public List<PollutionDto> showAirPollutionByLocation() {
         int startPage = 1;
         int maxPage = 30;
         String url = "http://openapi.seoul.go.kr:8088/7246436c7a746e733437454d506762/json/ListAirQualityByDistrictService/"
-                        + startPage + "/" + maxPage;
+                + startPage + "/" + maxPage;
 
         try {
-            log.info("\n=== api 호출 후 데이터 저장 로직 시작 ===\n");
+            log.info("\n========= parkDtoList 초기화 ===========");
+            log.info("\n=== 대기현황 api 호출 후 데이터 저장 로직 시작 ===");
             pollutionDtoList.clear();
+
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+            status = responseEntity.getStatusCode();
+            if (status == HttpStatus.OK
+                    || status == HttpStatus.CREATED
+                    || status == HttpStatus.ACCEPTED) { // 비동기 callback 아직 처리안함
 
-            // API 호출 후 JSON 데이터 파싱
-            JSONObject jsonObject = (JSONObject) new JSONParser().parse(responseEntity.getBody());
-            JSONObject jsonData = (JSONObject) jsonObject.get("ListAirQualityByDistrictService");
-            JSONArray totalData = (JSONArray) jsonData.get("row");
+                System.out.println("HttpStatus : " + status);
 
-            for(Object data : totalData){
-                JSONObject object = (JSONObject) data;
-                PollutionDto pollutionDto = makeDtoByJsonData(object);
-                pollutionDtoList.add(pollutionDto);
-                pollutionRepository.save(pollutionDto.toEntity());
+                // API 호출 후 JSON 데이터 파싱
+                JSONObject jsonObject = (JSONObject) new JSONParser().parse(responseEntity.getBody());
+                JSONObject jsonData = (JSONObject) jsonObject.get("ListAirQualityByDistrictService");
+                JSONObject jsonResult = (JSONObject) jsonData.get("RESULT");
+                JSONArray totalData = (JSONArray) jsonData.get("row");
+
+                String code = (String) jsonResult.get("CODE");
+
+                if (jsonObject != null && code.equals(RESULT_CODE)) {
+                    for (Object data : totalData) {
+                        JSONObject object = (JSONObject) data;
+                        PollutionDto pollutionDto = makeDtoByJsonData(object);
+                        pollutionDtoList.add(pollutionDto);
+                        pollutionRepository.save(pollutionDto.toEntity());
+                    }
+                } else {
+                    log.warn("API 호출 실패");
+                    System.out.println("요청 결과 데이터 오류. 코드값 : " + code);
+                    return null;
+                }
+            } else {
+                log.warn("API 호출 실패");
+                System.out.println("API 호출 실패. 상태코드 : " + status);
+                return null;
             }
         } catch (Exception e) {
             log.warn("초기 API 호출 중 오류 발생");
-            e.printStackTrace();
+            System.out.println("오류 메시지 : " + e.getMessage());
+            return null;
         }
         log.info("\n=== 대기현황 api 호출 끝 ===\n");
-        for(PollutionDto pollutionDto : pollutionDtoList)
+        for (PollutionDto pollutionDto : pollutionDtoList)
             System.out.println(pollutionDto);
         System.out.println("\n=============================================\n");
 
         return pollutionDtoList;
     }
 
-    private static PollutionDto makeDtoByJsonData(JSONObject pollution){
+
+    private static PollutionDto makeDtoByJsonData(JSONObject pollution) {
         // 측정시각 기존 데이터 : ex) 202401261100, parsing 후 => 2024년 01월 26일 11시
-        String dateStr = (String)pollution.get("MSRDATE");
+        String dateStr = (String) pollution.get("MSRDATE");
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
         String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분"));
@@ -83,8 +108,8 @@ public class PollutionService {
                 format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시"));
 
         // 대시 상태 등급 정보 : [점검 중 일때 공백으로 입력됨 => 점검 중으로 변환]
-        String grade = ((String)pollution.get("GRADE")).equals("")
-                            ? "점검중" : (String)pollution.get("GRADE");
+        String grade = ((String) pollution.get("GRADE")).equals("")
+                ? "점검중" : (String) pollution.get("GRADE");
 
 
         PollutionDto pollutionDto = PollutionDto.builder()
@@ -92,7 +117,7 @@ public class PollutionService {
                 .msrName((String) pollution.get("MSRSTENAME"))
                 .msrDate(parsedDate)
                 .grade(grade)
-                .pm10((String)pollution.get("PM10"))
+                .pm10((String) pollution.get("PM10"))
                 .pm25((String) pollution.get("PM25"))
                 .updateTime(now)
                 .build();
