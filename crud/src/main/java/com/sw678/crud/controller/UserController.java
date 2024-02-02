c351d4a50b2a99355a35f3d8e140e25719299508
박태현 <lcecoffee@naver.com>
2024-02-02 15:09:46 +0900

    Feat : CRUD 회원가입시 CORS 오류발생
    MSA 등록 구현
    회원가입시 CORS 오류 발생
    
    02.02 15:09

--- crud/src/main/java/com/sw678/crud/controller/UserController.java
+++ crud/src/main/java/com/sw678/crud/controller/UserController.java
@@ -11,10 +11,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
