dc8eba0a2d2049db67c7fcd4867248069842fba9
Seungchuulee <seungrowth@gmail.com>
2024-02-02 10:59:33 +0900

    Feat : 회원가입시 인증번호를 통한 이메일 확인 추가

--- crud/src/main/java/com/sw678/crud/controller/UserController.java
+++ crud/src/main/java/com/sw678/crud/controller/UserController.java
@@ -6,38 +6,52 @@ import com.sw678.crud.model.entity.User;
 import com.sw678.crud.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final JavaMailSender mailSender;
+    // private JavaMailSender mailSender;
+    private static final String FROM_ADDRESS = "sangjuncho232@gmail.com";
+
+       /* @Autowired
+        public UserController(UserService userService, JavaMailSender mailSender) {
+            this.userService = userService;
+            this.mailSender = mailSender;
+        }*/
 
     @GetMapping("/signin")
-    public String signinForm(Model model){
+    public String signinForm(Model model) {
         model.addAttribute("signinDto", new SignupDto());
 
         return "signinForm";
     }
 
     @PostMapping("/signin")
-    public String signinForm(@Valid SigninDto signinDto, BindingResult bindingResult){
+    public String signinForm(@Valid SigninDto signinDto, BindingResult bindingResult) {
 
         // 검증 실패시
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "signinForm";
         }
 
         // 로그인 시도
         User loginUser = userService.login(signinDto);
 
-        if(loginUser == null){
+        if (loginUser == null) {
             bindingResult.reject("loginFail", "아이디나 비밀번호가 일치하지 않습니다.");
             return "signinForm";
         }
@@ -49,17 +63,17 @@ public class UserController {
 
     // 회원가입
     @GetMapping("/signup")
-    public String signupForm(Model model){
+    public String signupForm(Model model) {
         model.addAttribute("signupDto", new SignupDto());
 
         return "signupForm";
     }
 
     @PostMapping("/signup")
-    public String signup(@Valid @ModelAttribute SignupDto signupDto, BindingResult bindingResult){
+    public String signup(@Valid @ModelAttribute SignupDto signupDto, BindingResult bindingResult) {
 
         // 검증 오류 확인
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "signupForm";
         }
 
@@ -68,6 +82,44 @@ public class UserController {
         userService.signup(user);
 
         return "redirect:/signin";
+    }
+
+    //////////////////////////////////////
+    //// MAIL
+
+    @ResponseBody
+    @GetMapping("/emailAuth")
+    public String emailAuth(String email) {
+        Random random = new Random();
+        int check = (int) (Math.random() * 899999) + 100000;
+
+
+        try {
+            SimpleMailMessage message = createMessageForm(email, check);
+            mailSender.send(message);
+        } catch (Exception e) {
+            System.out.println("에러 : " + e.getMessage());
+        }
+
+        return Integer.toString(check);
+    }
+
+    private SimpleMailMessage createMessageForm(String email, int check) throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        String sender = FROM_ADDRESS;
+        String receiver = email;
+        String title = "678서비스 회원가입 인증 메일";
+        String content = "안녕하세요 678 서비스입니다.\n\n"
+                + "인증번호 : " + check
+                + "\n\n678페이지로 돌아가 해당 인증번호를 기입하여 주세요.";
+
+        message.setFrom(sender);
+        message.setTo(receiver);
+        message.setSubject(title);
+        message.setText(content);
+
+        return message;
     }
 
 
