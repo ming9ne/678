0c183078bc1d52fc4a12417551571a140f0377fc
박태현 <lcecoffee@naver.com>
2024-02-02 11:25:54 +0900

    Feat : board test
    02.02 11:25
    board test

--- board/src/main/java/com/sw678/board/BoardApplication.java
+++ board/src/main/java/com/sw678/board/BoardApplication.java
@@ -3,6 +3,7 @@ package com.sw678.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 0202 1124 test
 @SpringBootApplication
 public class BoardApplication {
 
